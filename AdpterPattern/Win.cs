00b31fb99a82d7d06b016cf754a66c2c3f856daf
TalKehila <129615478+TalKehila@users.noreply.github.com>
2024-04-27 22:28:54 +0300

    Adding adpater pattern

---
+++ AdpterPattern/Win.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdpterPattern
+{
+	internal class Win:IWin
+	{
+		public void WinUse() => Console.WriteLine("I using Windows"); 
+	}
+}
