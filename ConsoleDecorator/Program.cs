3eff43e120290c6c2a7399c4023c9eebe285c903
TalKehila <129615478+TalKehila@users.noreply.github.com>
2024-05-11 23:39:50 +0300

    Derctor

---
+++ ConsoleDecorator/Program.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleDecorator.Additions;
+using ConsoleDecorator.Bases;
+
+namespace ConsoleDecorator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizza pizza = new Italian();
+            pizza = new Tomato(pizza);
+            pizza = new Tomato(pizza);
+            PrintCheck(pizza);
+
+            Pizza pizzaB = new Bulgarian();
+            pizzaB = new Cheese(pizzaB);
+            pizzaB = new Tomato(pizzaB);
+            PrintCheck(pizzaB);
+
+            Pizza pizzaC = new Bulgarian();
+            pizzaC = new Italian();
+            pizzaC = new Tomato(pizzaC);
+            PrintCheck(pizzaC);
+        }
+
+        private static void PrintCheck(Pizza pizza) => Console.WriteLine($"{pizza.Name}: {pizza.Cost} ");
+    }
+}
